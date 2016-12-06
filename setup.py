f423f58949a550375f9a2cb002e148ee7b306135
Jenkins <jenkins@opennodecloud.com>
2016-12-06 11:55:43 +0000

    Preparing new release: 0.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 
 setup(
     name='nodeconductor-aws',
-    version='0.1.0.dev0',
+    version='0.1.0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://nodeconductor.com',
